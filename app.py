447a16da074197520466f3a72e61a3421b940b26
reubence <reuben3699@gmail.com>
2020-04-05 20:56:28 +0530

    web

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 
 @app.route("/analyze", methods=['POST'])
 def analyze():
-    some_json=request.get_json()
+    #some_json=request.get_json()
    
-    text = some_json['text']  
+    #text = some_json['text']  
     #response = chatgui.chatbot_response(text)
     return str("You have reached us")
 
--- chatgui.py
+++ chatgui.py
@@ -64,24 +64,3 @@ def chatbot_response(msg):
     return res
 
 
-#Creating GUI with tkinter
-import tkinter
-from tkinter import *
-
-
-def send():
-    msg = EntryBox.get("1.0",'end-1c').strip()
-    EntryBox.delete("0.0",END)
-
-    if msg != '':
-        ChatLog.config(state=NORMAL)
-        ChatLog.insert(END, "You: " + msg + '\n\n')
-        ChatLog.config(foreground="#442265", font=("Verdana", 12 ))
-    
-        res = chatbot_response(msg)
-        ChatLog.insert(END, "Bot: " + res + '\n\n')
-            
-        ChatLog.config(state=DISABLED)
-        ChatLog.yview(END)
- 
-
